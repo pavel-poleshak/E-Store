7b6a811664d58d8219089b7344bcb583853f02a7
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 00:10:06 +0300

    Added Product Class

---
+++ E-Store/E-Store.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Entities
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
