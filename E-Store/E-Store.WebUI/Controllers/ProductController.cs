c46bf12a22bfead3f229e72b30a5d4b3c5d8a883
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 00:27:33 +0300

    Added ProductController

---
+++ E-Store/E-Store.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Store.Domain.Abstract;
+using E_Store.Domain.Entities;
+
+namespace E_Store.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository productRepository)
+        {
+            repository = productRepository;
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
