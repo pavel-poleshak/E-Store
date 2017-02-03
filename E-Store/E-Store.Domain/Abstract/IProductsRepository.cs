c41277aa2763a644bb5cc362250a7dbd0a295487
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 00:13:22 +0300

    Added  IProductsRepository

---
+++ E-Store/E-Store.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Store.Domain.Entities;
+
+namespace E_Store.Domain.Abstract
+{
+    interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
