699ab6dad881da4437ccfc9dad19be1ca025892e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 23:25:01 +0300

    Added DB Context

---
+++ E-Store/E-Store.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,20 @@
+﻿using E_Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Concrete
+{
+    public class EFDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public EFDbContext():base("EStoreDB")
+        {
+
+        }
+    }
+}
