e2479391482aa4af4da31db91826b19412524700
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-05 00:32:52 +0300

    Added Paginator in Method List

--- E-Store/E-Store.WebUI/Controllers/ProductController.cs
+++ E-Store/E-Store.WebUI/Controllers/ProductController.cs
@@ -11,14 +11,18 @@ namespace E_Store.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        private int pageSize = 4;
         public ProductController(IProductsRepository productRepository)
         {
             repository = productRepository;
         }
         // GET: Product
-        public ActionResult List()
+        public ActionResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize));
         }
     }
 }
