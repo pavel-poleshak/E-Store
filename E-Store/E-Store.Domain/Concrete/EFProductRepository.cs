31eb57ffcf212ec68cd6f5bb0f29b65a1c5738d3
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 23:28:54 +0300

    Added EFProductRepository

---
+++ E-Store/E-Store.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using E_Store.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Store.Domain.Entities;
+
+namespace E_Store.Domain.Concrete
+{
+    public class EFProductRepository : IProductsRepository
+    {
+        EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return context.Products;
+            }
+        }
+    }
+}
