7590a73a4f848e51f960fe6ea1cd5d9e558cc12a
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-04 23:05:10 +0300

    RouteConfig Changed

--- E-Store/E-Store.WebUI/App_Start/RouteConfig.cs
+++ E-Store/E-Store.WebUI/App_Start/RouteConfig.cs
@@ -14,10 +14,15 @@ namespace E_Store.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+           );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
