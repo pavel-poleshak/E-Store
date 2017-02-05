bbfef541bda0248ce07a2b8575cf71163a2f4abf
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-05 20:34:40 +0300

    Added routes

--- E-Store/E-Store.WebUI/App_Start/RouteConfig.cs
+++ E-Store/E-Store.WebUI/App_Start/RouteConfig.cs
@@ -13,17 +13,34 @@ namespace E_Store.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page/{page}",
-                defaults: new { controller = "Product", action = "List" }
-                );
+            routes.MapRoute(null,
+                 "",
+                 new
+                 {
+                     controller = "Product",
+                     action = "List",
+                     category = (string)null,
+                     page = 1
+                 }
+             );
 
             routes.MapRoute(
-               name: "Default",
-               url: "{controller}/{action}/{id}",
-               defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
-           );
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Product", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
             //routes.MapRoute(
             //    name: "Default",
             //    url: "{controller}/{action}/{id}",
