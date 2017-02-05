0904125a8c3cd894c8708fc82332153b642fe634
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-06 00:53:45 +0300

    Added Nav Controller

---
+++ E-Store/E-Store.WebUI/Controllers/NavController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Store.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public string Menu()
+        {
+            return "Контроллер Nav";
+        }
+    }
+}
