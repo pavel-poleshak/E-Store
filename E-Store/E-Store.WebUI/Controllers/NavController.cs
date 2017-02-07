ca37d863e6dc4b2f5693093f827690d127040f11
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-07 22:03:56 +0300

    Added ViewBag property for selected category

--- E-Store/E-Store.WebUI/Controllers/NavController.cs
+++ E-Store/E-Store.WebUI/Controllers/NavController.cs
@@ -16,11 +16,12 @@ namespace E_Store.WebUI.Controllers
         }
         // GET: Nav
         [ChildActionOnly]
-        public PartialViewResult Menu()
+        public PartialViewResult Menu(string category = null)
         {
+            ViewBag.SelectedCategory = category;
             IEnumerable<string> categories = repository.Products
                 .Select(p => p.Category)
-                .Where(p=>p!=null && p!="")
+                .Where(p => p != null && p != "")
                 .Distinct()
                 .OrderBy(x => x);
             return PartialView(categories);
