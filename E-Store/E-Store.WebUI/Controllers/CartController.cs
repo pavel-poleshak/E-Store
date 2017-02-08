378d457ef4b8c8730ab8c2385d19f9aa5c62f229
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-09 00:54:07 +0300

    Added CartController

---
+++ E-Store/E-Store.WebUI/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using E_Store.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Store.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        IProductsRepository repository;
+
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
