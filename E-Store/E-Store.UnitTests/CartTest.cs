41868782790a00beddea291a552a9ae858697b4a
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-08 23:49:58 +0300

    Added Test For AddItems Method
    When add product to existing cart item

--- E-Store/E-Store.UnitTests/CartTest.cs
+++ E-Store/E-Store.UnitTests/CartTest.cs
@@ -26,5 +26,31 @@ namespace E_Store.UnitTests
             Assert.AreEqual(items[0].Product, p1);
             Assert.AreEqual(items[1].Product, p2);
         }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Items()
+        {
+            //arrange
+            Product p1 = new Product() { ProductId = 2, Name = "Kaspersky AV" };
+            Product p2 = new Product() { ProductId = 1, Name = "NOD32" };
+            Cart cart = new Cart();
+
+            //act
+            cart.AddItem(p1, 1);
+            cart.AddItem(p2, 1);
+            cart.AddItem(p1, 2);
+
+            List<CartItem> items = cart.Items.OrderBy(c => c.Product.ProductId).ToList();
+
+            //assert
+
+            Assert.AreEqual(items[0].Product, p2);
+            Assert.AreEqual(items[0].Quantity, 1);
+            Assert.AreEqual(items[1].Product, p1);
+            Assert.AreEqual(items[1].Quantity, 3);
+
+
+
+        }
     }
 }
