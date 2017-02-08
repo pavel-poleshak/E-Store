0a6cd6150920ed372cff83ec2df6562bec349a0d
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-08 23:14:46 +0300

    Added Cart class, AddItem method

---
+++ E-Store/E-Store.Domain/Entities/Cart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartItem> cartItems = new List<CartItem>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartItem items = cartItems
+                .Where(p => p.Product.ProductId == product.ProductId)
+                .FirstOrDefault();
+
+            if (items==null)
+            {
+                cartItems.Add(new CartItem()
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                items.Quantity += quantity;              
+            }
+        }
+    }
+}
