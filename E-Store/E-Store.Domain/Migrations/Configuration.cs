289c49e656ab41375f65ff37e0fce5850f03856e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-14 00:01:40 +0300

    Enabled migrations

---
+++ E-Store/E-Store.Domain/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace E_Store.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<E_Store.Domain.Concrete.EFDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "E_Store.Domain.Concrete.EFDbContext";
+        }
+
+        protected override void Seed(E_Store.Domain.Concrete.EFDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
