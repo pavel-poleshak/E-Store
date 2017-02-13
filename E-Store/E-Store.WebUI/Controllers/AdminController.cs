90170088629a5864fdcf01b99bcc40ac7f9fc20a
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-02-13 11:36:28 +0300

    Added Admin Controller

---
+++ E-Store/E-Store.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using E_Store.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Store.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductsRepository repository;
+
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
