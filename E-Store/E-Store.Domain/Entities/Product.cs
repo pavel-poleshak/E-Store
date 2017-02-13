d45331aa8f00cbfe23bd141b5f420cc78b4dac1f
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-13 20:16:53 +0300

    Added Display attributes to properties

--- E-Store/E-Store.Domain/Entities/Product.cs
+++ E-Store/E-Store.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace E_Store.Domain.Entities
     public class Product
     {
         public int ProductId { get; set; }
+        [Display(Name ="Наименование")]
         public string Name { get; set; }
+        [Display(Name ="Описание")]
         public string Description { get; set; }
+        [Display(Name ="Цена")]
         public decimal Price { get; set; }
+        [Display(Name ="Категория")]
         public string Category { get; set; }
     }
 }
