b3c436a9be312145871f95a053e784d71b05be86
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-02-13 11:08:03 +0300

    Added Checkout method and View

--- E-Store/E-Store.WebUI/Controllers/CartController.cs
+++ E-Store/E-Store.WebUI/Controllers/CartController.cs
@@ -53,5 +53,10 @@ namespace E_Store.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
