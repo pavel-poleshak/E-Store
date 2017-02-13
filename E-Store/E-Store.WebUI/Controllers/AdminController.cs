139de340989568e70c670fce0ade442422e8d481
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-13 22:10:40 +0300

    Added POST method Edit

--- E-Store/E-Store.WebUI/Controllers/AdminController.cs
+++ E-Store/E-Store.WebUI/Controllers/AdminController.cs
@@ -27,5 +27,21 @@ namespace E_Store.WebUI.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
             return View(product);
         }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("Изменение к товару {0} были успешно применены.", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+
+        }
     }
 }
