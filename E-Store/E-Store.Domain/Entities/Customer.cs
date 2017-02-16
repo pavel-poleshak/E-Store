a63661ce9829442bc0db99f2fda7edb9fad754df
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-17 00:26:39 +0300

    Added Customer class
    Orders property need for relation one to many with Orders table

---
+++ E-Store/E-Store.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Entities
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
