456b4c2e410de947873333a97085d9b408ea126f
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-18 13:14:40 +0300

    Addded OrderLine entity with interface

---
+++ E-Store/E-Store.Domain/Abstract/IOrderLineRepository.cs
@@ -0,0 +1,17 @@
+﻿using E_Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Abstract
+{
+    public interface IOrderLineRepository
+    {
+        IQueryable<OrderLine> GetAll();
+      
+        void Create(OrderLine item);
+
+    }
+}
---
+++ E-Store/E-Store.Domain/Concrete/EFOrderLineRepository.cs
@@ -0,0 +1,28 @@
+﻿using E_Store.Domain.Abstract;
+using E_Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Concrete
+{
+    public class EFOrderLineRepository : IOrderLineRepository
+    {
+        EFDbContext context;
+
+        public void Create(OrderLine item)
+        {
+            if (item!=null)
+            {
+                context.OrderLines.Add(item);
+            };
+        }
+
+        public IQueryable<OrderLine> GetAll()
+        {
+            return context.OrderLines;
+        }
+    }
+}
