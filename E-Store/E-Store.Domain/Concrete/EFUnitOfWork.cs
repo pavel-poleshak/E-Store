c3bfbbacb55ce18c08b09f8d465b84f411a17cff
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-18 14:53:49 +0300

    Added EFUnitOfWork class
    contains all repo

---
+++ E-Store/E-Store.Domain/Concrete/EFUnitOfWork.cs
@@ -0,0 +1,83 @@
+﻿using E_Store.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Store.Domain.Entities;
+
+namespace E_Store.Domain.Concrete
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private EFDbContext context;
+        private EFCustomerRepository customerRepository;
+        private EFProductRepository productRepository;
+        private EFOrderRepository orderRepository;
+        private EFOrderLineRepository orderLineRepository;
+        
+        public EFUnitOfWork(EFDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IRepository<Customer> Customers
+        {
+            get
+            {
+                return customerRepository ?? new EFCustomerRepository(context);
+            }
+        }
+
+        public IOrderLineRepository OrderLines
+        {
+            get
+            {
+                return orderLineRepository ?? new EFOrderLineRepository(context);
+            }
+        }
+
+        public IRepository<Order> Orders
+        {
+            get
+            {
+                return orderRepository ?? new EFOrderRepository(context);
+            }
+        }
+
+        public IRepository<Product> Products
+        {
+            get
+            {
+                return productRepository ?? new EFProductRepository(context);
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
