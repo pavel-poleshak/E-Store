d86e9db2f1b806dc5785b6ddb15cd5d896a9ed36
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-18 13:24:41 +0300

    Added IUnitOfWork
    Interface for UoF pattern

---
+++ E-Store/E-Store.Domain/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using E_Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Store.Domain.Abstract
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IRepository<Customer> Customers { get; }
+        IRepository<Product> Products { get; }
+        IRepository<Order> Orders { get; }
+        IRepository<OrderLine> OrderLines { get; }
+        void Save();
+    }
+}
