24baa10646533ac2a002f749562327b7bb5fd84a
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-20 12:54:18 +0300

    Renamed views, Added Checkou model

---
+++ E-Store/E-Store.WebUI/Models/CheckoutViewModel.cs
@@ -0,0 +1,14 @@
+﻿using E_Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Store.WebUI.Models
+{
+    public class CheckoutViewModel
+    {
+        public Customer Customer { get; set; }
+        public ShippingDetails ShippingDetails { get; set; } 
+    }
+}
