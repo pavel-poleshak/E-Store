4937ea3bed0c9686006093a554c256c6a31d3a19
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-02-23 22:15:57 +0300

    Added Bootstrap-select plugin
    customize native select

---
+++ E-Store/E-Store.WebUI/HtmlHelpers/DropdownForCategoriesHelper.cs
@@ -0,0 +1,42 @@
+﻿using E_Store.Domain.Entities;
+using E_Store.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Store.WebUI.HtmlHelpers
+{
+    public static class DropdownForCategoriesHelper
+    {
+        public static MvcHtmlString CreateDropdownForProductCategories(this HtmlHelper htmlHelper, IEnumerable<SubCategoryDTO> listSubCategories)
+        {
+            StringBuilder result = new StringBuilder();
+            TagBuilder select = new TagBuilder("select");
+            foreach (SubCategoryDTO category in listSubCategories)
+            {
+
+                TagBuilder optGroup = new TagBuilder("optgroup");
+                optGroup.MergeAttribute("label", category.CategoryName);
+                foreach (SubCategory subCategory in category.SubCategories)
+                {
+                    TagBuilder option = new TagBuilder("option");
+                    option.MergeAttribute("value", subCategory.Id.ToString());
+                    option.InnerHtml = subCategory.Name;
+                    optGroup.InnerHtml += option;
+                }
+                select.InnerHtml += optGroup;                
+            }
+            select.AddCssClass("selectpicker show-tick");
+            select.MergeAttribute("data-live-search", "true");
+            select.MergeAttribute("data-header", "Выберите категорию");
+            select.MergeAttribute("data-size", "8");
+            result.Append(select.ToString());
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+    }
+}
